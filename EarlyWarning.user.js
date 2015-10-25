2f778d48783108573b9cc5e9aa71fbdcec07b12a
Normal Human <normalhuman@users.noreply.github.com>
2015-10-24 21:32:51 -0400

    all-latex title warning; refactoring

--- EarlyWarning.user.js
+++ EarlyWarning.user.js
@@ -5,32 +5,26 @@
 // @match       *://stackexchange.com/search
 // @grant       none
 // @run-at      document-end
-// @version     15.10.10
+// @version     15.10.11
 // ==/UserScript==
 
-// runs only if a browser is pointed at the page //stackexchange.com/search#bot
+// runs only if the browser is pointed at //stackexchange.com/search#bot
 
 if (window.location.hash === '#bot') {
   var token = 'ACCESS_TOKEN';    // get access token from the url hash at https://stackexchange.com/oauth/dialog?client_id=5748&scope=no_expiry,write_access&redirect_uri=https://stackexchange.com/
   var apiKey = 'vURDLnkgkrLc7qAq)D89tA(('; 
   var site = 'math';
   var siteUrl = 'http://math.stackexchange.com';
-  var qId, topTag, comment, linkGood;
   var commented = [];
   
-  // most popular tags are whitelisted to reduce API requests:  
-  var popular = ['calculus', 'real-analysis', 'linear-algebra', 'probability', 'abstract-algebra', 'general-topology', 'combinatorics', 'complex-analysis', 
-                 'algebra-precalculus', 'geometry', 'functional-analysis', 'number-theory', 'differential-equations', 'elementary-number-theory', 'limits', 
-                 'probability-theory', 'measure-theory', 'statistics', 'multivariable-calculus', 'elementary-set-theory'];
-  
-  // These are rare (<1000 questions) but okay: 
-  var okay = ['automata', 'analytic-number-theory', 'boolean-algebra', 'calculus-of-variations', 'coding-theory', 'computability', 'complex-geometry', 'formal-languages',
-              'game-theory', 'harmonic-analysis', 'homological-algebra', 'homotopy-theory', 'laplace-transform', 'linear-programming', 'mathematical-physics', 
-              'model-theory', 'numerical-linear-algebra', 'order-theory', 'predicate-logic', 'propositional-calculus', 'puzzle', 'regular-language', 'stochastic-calculus'];  
+  // Tags with 1500+ questions: 
+  var popular = ['calculus', 'real-analysis', 'linear-algebra', 'probability', 'abstract-algebra', 'integration', 'sequences-and-series', 'general-topology', 'combinatorics', 'matrices', 'complex-analysis', 'group-theory', 'algebra-precalculus', 'analysis', 'geometry', 'functional-analysis', 'number-theory', 'differential-equations', 'elementary-number-theory', 'limits', 'probability-theory', 'measure-theory', 'statistics', 'multivariable-calculus', 'functions', 'discrete-mathematics', 'elementary-set-theory', 'trigonometry', 'algebraic-geometry', 'differential-geometry', 'derivatives', 'inequality', 'reference-request', 'logic', 'polynomials', 'graph-theory', 'probability-distributions', 'ring-theory', 'pde', 'algebraic-topology', 'proof-strategy', 'convergence', 'commutative-algebra', 'optimization', 'proof-verification', 'vector-spaces', 'definite-integrals', 'soft-question', 'complex-numbers', 'algorithms', 'summation', 'metric-spaces', 'stochastic-processes', 'finite-groups', 'numerical-methods', 'notation', 'category-theory', 'prime-numbers', 'fourier-analysis', 'field-theory', 'proof-writing', 'continuity', 'eigenvalues-eigenvectors', 'permutations', 'set-theory', 'induction', 'modular-arithmetic', 'logarithms', 'recurrence-relations', 'terminology', 'modules', 'representation-theory', 'operator-theory', 'asymptotics', 'arithmetic', 'random-variables', 'algebraic-number-theory', 'manifolds', 'power-series', 'convex-analysis', 'computer-science', 'hilbert-spaces', 'galois-theory', 'binomial-coefficients', 'improper-integrals', 'differential-topology', 'definition', 'contest-math', 'vectors', 'banach-spaces', 'self-learning', 'special-functions', 'divisibility', 'exponential-function', 'taylor-expansion', 'lie-groups', 'diophantine-equations', 'lebesgue-integral', 'normal-distribution', 'fourier-series', 'ideals', 'dynamical-systems', 'euclidean-geometry', 'physics', 'lie-algebras', 'determinant', 'compactness', 'recreational-mathematics', 'analytic-geometry', 'roots', 'systems-of-equations', 'norm', 'circle', 'riemannian-geometry', 'relations', 'education', 'examples-counterexamples', 'graphing-functions', 'intuition', 'sobolev-spaces', 'indefinite-integrals', 'triangle', 'convex-optimization', 'exponentiation', 'markov-chains', 'partial-derivative', 'finite-fields'];
+
+  // Okay as a top tag: either popular or otherwise sufficient
+  var okay = popular.concat(['automata', 'analytic-number-theory', 'boolean-algebra', 'calculus-of-variations', 'coding-theory', 'computability', 'complex-geometry', 'formal-languages', 'game-theory', 'harmonic-analysis', 'homological-algebra', 'homotopy-theory', 'laplace-transform', 'linear-programming', 'mathematical-physics', 'model-theory', 'numerical-linear-algebra', 'order-theory', 'predicate-logic', 'propositional-calculus', 'puzzle', 'regular-language', 'stochastic-calculus']);  
   
   // These should not be used on their own
-  var vague = ['advice', 'big-list', 'book-recommendation', 'contest-math', 'definition', 'norm', 'notation', 'proof-strategy', 'proof-verification', 
-               'proof-writing', 'reference-request', 'soft-question', 'terminology', 'transformation'];
+  var vague = ['advice', 'big-list', 'book-recommendation', 'contest-math', 'definition', 'examples-counterexamples', 'norm', 'notation', 'proof-strategy', 'proof-verification', 'proof-writing', 'reference-request', 'soft-question', 'terminology', 'transformation'];
   
   // Suggest replacements for these: 
   var replaceTag = ['analysis']; 
@@ -46,7 +40,7 @@ function startup() {
   ws.onmessage = function(e) {
     var realData = JSON.parse(e.data).data;
     if (realData !== 'hb') {
-      processQuestion(JSON.parse(realData));
+      window.setTimeout(processQuestion, 15000, JSON.parse(realData).id);
     }
   };
   ws.onopen = function() { ws.send('69-questions-newest'); };
@@ -56,100 +50,110 @@ function startup() {
 }
 
 
-function processQuestion(data) {
-  qId = data.id;
-  var title = data.body.split('"question-hyperlink">')[1].split('</a>')[0];
-  comment = '';
-  linkGood = false; 
-  commentOnTitle(title);
-  window.setTimeout(commentOnTags, 15000, data.tags);  
-}  
-
-
-function commentOnTags(tags) {
-  topTag = tags[0]; 
-  var replacing = replaceTag.indexOf(topTag);
-  if (popular.indexOf(topTag) > -1 || okay.indexOf(topTag) > -1) {
-    commentOnBody();
-  }
-  else if (tags.indexOf('self-learning') > -1 && arraysDisjoint(tags, ['soft-question', 'career-development', 'education', 'teaching', 'advice', 'book-recommendation', 'reference-request', 'big-list'])) {
-    comment = comment + "Please don't use (self-learning) tag just because you were self-studying when you came across this question. This tag is only for questions *about the process of self-studying*. ";
-    commentOnBody();
-  }
-  else if (replacing > -1 && arraysDisjoint(tags, replacements[replacing])) {
-    comment = comment + 'Consider replacing (' + topTag + ') with a more specific tag, such as ' + replacements[replacing].slice(0,3).map(function(a) {return '('+a+')';}).join(', ') + '... ';
-    commentOnBody();
-  }
-  else if (vague.indexOf(topTag) > -1 && tags.length == 1) {
-    comment = comment + 'Tag ('+topTag+') should not be the only tag a question has. Please add a tag for a subject area to which the question belongs. ';
-    commentOnBody();
-  }
-  else {
-    var filter = '!GeBU7l0z-7CFD';
-    var request = '//api.stackexchange.com/2.2/tags/'+topTag+'/info?order=desc&sort=popular&site='+site+'&filter='+filter+'&key='+apiKey;
-    $.ajax({url: request, dataType: 'json', method: 'GET'}).done(function(data) {
-      var count = data.items[0].count;
-      if (count > 0 && count < 1500) {  
-        comment = comment + 'Consider adding a tag for a broader subject area to which the question belongs. This will improve the visibility of your question. ';
+function processQuestion(id) {
+  var filter = '!w-1xUscA3.XwxFFOU4';
+  var request = '//api.stackexchange.com/2.2/questions/' + id + '?order=desc&sort=activity&site='+site+'&filter='+filter+'&key='+apiKey;
+  $.ajax({url: request, dataType: 'json', method: 'GET'}).done(function(data) {
+    var postData = data.items[0];
+    if (!postData) {
+      report = 'Failed to fetch:\n' + data;
+      console.log(report); 
+    }
+    else {
+      var comment = {};
+      comment.postId = postData.question_id; 
+      comment.text = '';
+      comment = commentOnTitle(postData.title, comment);
+      comment = commentOnBody(postData.body, comment);
+      comment = commentOnTags(postData.tags, comment);
+      if (comment.linkGood) {
+        comment.text = comment.text + 'See also: [How to ask a good question?](//meta.math.stackexchange.com/q/9959) ';
       }
-      commentOnBody();
-    });
-  }
+      sendComment(comment); 
+    }
+  });
 }
 
 
-function commentOnTitle(title) {
+function commentOnTitle(title, comment) {
   var badWords = title.match(/\b(anyone|difficult|doubt|hard|help|interesting|please|query|question|someone|task|tough)\b/ig); 
-  var badPunctuation = title.match(/\?{2,}/ig);
-  var linkGood = false;
+  var punctuationMatch = title.match(/\?{2,}/ig);
+  var tallReasons = []; 
   if (badWords && title.length <= 70) {
     var prepWords = '*' + badWords.join(', ').toLowerCase() + '*';
-    comment = comment + 'Words such as ' + prepWords + ' do not add information to titles. Please [edit] the title so that it better describes the specifics of your question. Do not hesitate to make it longer or include a formula if needed. ';
-    linkGood = true;
+    comment.text = comment.text + 'Words such as ' + prepWords + ' do not add information to titles. Please [edit] the title so that it better describes the specifics of your question. Do not hesitate to make it longer or include a formula if needed. ';
+    comment.linkGood = true;
   }
-  if (badPunctuation) {
-    comment = comment + 'Please remove excessive punctuation such as "' + badPunctuation[0] + '". ';
-    linkGood = true;
+  if (punctuationMatch) {
+    comment.text = comment.text + 'Please remove excessive punctuation such as "' + punctuationMatch[0] + '". ';
+    comment.linkGood = true;
+    comment.badPunctuation = true; 
   }
   if (/\\dfrac/.test(title)) {
-    comment = comment + 'Please replace `\\dfrac` with `\\frac` in the title; tall formulas in titles break the layout of question lists. ';
+    tallReasons.push('replace `\\dfrac` with `\\frac`');
   }
-  else if (/\\displaystyle/.test(title)) {
-    comment = comment + 'Please remove `\\displaystyle` from the title; tall formulas in titles break the layout of question lists. ';
+  if (/\\displaystyle/.test(title)) {
+    tallReasons.push('remove `\\displaystyle`');
   }  
-  else if (/(\\int|\\sum)\s*\\limits/.test(title)) {
-    comment = comment + 'Please remove `\\limits` from the title; tall formulas in titles break the layout of question lists. ';
-  }  
+  if (/(\\int|\\sum)\s*\\limits/.test(title)) {
+    tallReasons.push('remove `\\limits`');
+  }
+  if (tallReasons.length > 0) {
+    comment.text = comment.text + 'Tall formulas in titles break the layout of question lists. Please ' + tallReasons.join(', ') + ' in the title. ';
+  }
+  if (/^\$[^$]*\$$/.test(title)) {
+    comment.text = comment.text + 'A title should not be all-MathJax; having some plain text helps with search and navigation. ';
+  }
+  return comment;
 }
 
 
-function commentOnBody() {
-  var filter = '!GeEyUcJFJO6t)';
-  var request = '//api.stackexchange.com/2.2/questions/' + qId + '?order=desc&sort=activity&site='+site+'&filter='+filter+'&key='+apiKey;
-  $.ajax({url: request, dataType: 'json', method: 'GET'}).done(function(data) {
-    var body = data.items[0].body;
-    var math = body.match(/&lt;|&gt;|[*^]|\/\d|\b(sin|cos|tan|exp|log|ln|sqrt|pi)\b/g);
-    if (!/\$/.test(body) && math && math.length >= 5) {
-      comment = comment + 'Formulas should be MathJax-formatted: see [math notation guide](//math.stackexchange.com/help/notation). ';
-      linkGood = true;   
-    }
-    if (linkGood) {
-      comment = comment + 'See also: [How to ask a good question?](//meta.math.stackexchange.com/q/9959) ';
-    }
-    sendComment(); 
-  });  
+function commentOnBody(body, comment) {
+  body = body.replace(/<pre>[\s\S]*?<\/pre>/g, "pre");
+  body = body.replace(/<code>[\s\S]*?<\/code>/g, "code");
+  var math = body.match(/&lt;|&gt;|[*^]|\/\d|\b(sin|cos|tan|exp|log|ln|sqrt|pi)\b/g);
+  if (!/\$/.test(body) && math && math.length >= 5) {
+    comment.text = comment.text + 'Formulas should be MathJax-formatted: see [math notation guide](//math.stackexchange.com/help/notation). ';
+    comment.linkGood = true;   
+  }
+  var punctuationMatch = body.match(/\?{2,}/ig);
+  if (!comment.badPunctuation && punctuationMatch) {
+    comment.text = comment.text + 'Please remove excessive punctuation such as "' + punctuationMatch[0] + '". ';
+    comment.linkGood = true;
+    comment.badPunctuation = true; 
+  }
+  return comment;
 }
 
 
-function sendComment() {
-  if (comment.length > 0 && commented.indexOf(qId) == -1) {
-    commented.push(qId);
-    comment = comment.slice(0,600);
+function commentOnTags(tags, comment) {
+  topTag = tags[0]; 
+  var replacing = replaceTag.indexOf(topTag);
+  if (tags.indexOf('self-learning') > -1 && arraysDisjoint(tags, ['soft-question', 'career-development', 'education', 'teaching', 'advice', 'book-recommendation', 'reference-request', 'big-list'])) {
+    comment.text = comment.text + "Please don't use (self-learning) tag just because you were self-studying. This tag is only for questions *about the process of self-studying*. ";
+  }
+  if (replacing > -1 && arraysDisjoint(tags, replacements[replacing])) {
+    comment.text = comment.text + 'Consider replacing (' + topTag + ') with a more specific tag, such as ' + replacements[replacing].slice(0,3).map(function(a) {return '('+a+')';}).join(', ') + '... ';
+  } 
+  else if (vague.indexOf(topTag) > -1 && tags.length == 1) {
+    comment.text = comment.text + 'Tag ('+topTag+') should not be the only tag a question has. Please add a tag for a subject area to which the question belongs. ';
+  }
+  else if (okay.indexOf(topTag) === -1) {
+    comment.text = comment.text + 'Consider adding a tag for a broader subject area to which the question belongs. This will improve the visibility of your question. ';
+  }
+  return comment;
+}
+
+
+function sendComment(comment) {
+  if (comment.text.length > 0 && commented.indexOf(comment.postId) === -1) {
+    commented.push(comment.postId);
+    var commentToPost = comment.text.slice(0,600);
     var filter = '!.UDq27j4ipL8j8W9';
-    var request = 'https://api.stackexchange.com/2.2/posts/'+qId+'/comments/add';
-    var payload = {site: site, key: apiKey, access_token: token, body: comment, preview: false, filter: filter};
+    var request = 'https://api.stackexchange.com/2.2/posts/' + comment.postId + '/comments/add';
+    var payload = {site: site, key: apiKey, access_token: token, body: commentToPost, preview: false, filter: filter};
     $.post(request, payload, handle, 'json');
-    var report = commented.length + '. Comment on ' + siteUrl + '/q/' + qId + '\n' + comment; 
+    var report = commented.length + '. Comment on ' + siteUrl + '/q/' + comment.postId + '\n' + commentToPost; 
     console.log(report);
   }
 }
