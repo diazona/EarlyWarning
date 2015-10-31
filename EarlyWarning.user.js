4ba9184d627086fe1db3f5d106e4793fda7843cd
Normal Human <normalhuman@users.noreply.github.com>
2015-10-31 19:38:26 -0400

    scale down tag warnings

--- EarlyWarning.user.js
+++ EarlyWarning.user.js
@@ -5,7 +5,7 @@
 // @match       *://stackexchange.com/search
 // @grant       none
 // @run-at      document-end
-// @version     15.10.13
+// @version     15.10.14
 // ==/UserScript==
 
 // runs only if the browser is pointed at //stackexchange.com/search#bot
@@ -18,18 +18,18 @@ if (window.location.hash === '#bot') {
   var SiteUrl = 'http://math.stackexchange.com';
   var Commented = [], MaxUser = 0, CommentId, PostId; 
   
-  // Tags with 1500+ questions: 
-  var Popular = ['calculus', 'real-analysis', 'linear-algebra', 'probability', 'abstract-algebra', 'integration', 'sequences-and-series', 'general-topology', 'combinatorics', 'matrices', 'complex-analysis', 'group-theory', 'algebra-precalculus', 'analysis', 'geometry', 'functional-analysis', 'number-theory', 'differential-equations', 'elementary-number-theory', 'limits', 'probability-theory', 'measure-theory', 'statistics', 'multivariable-calculus', 'functions', 'discrete-mathematics', 'elementary-set-theory', 'trigonometry', 'algebraic-geometry', 'differential-geometry', 'derivatives', 'inequality', 'reference-request', 'logic', 'polynomials', 'graph-theory', 'probability-distributions', 'ring-theory', 'pde', 'algebraic-topology', 'proof-strategy', 'convergence', 'commutative-algebra', 'optimization', 'proof-verification', 'vector-spaces', 'definite-integrals', 'soft-question', 'complex-numbers', 'algorithms', 'summation', 'metric-spaces', 'stochastic-processes', 'finite-groups', 'numerical-methods', 'notation', 'category-theory', 'prime-numbers', 'fourier-analysis', 'field-theory', 'proof-writing', 'continuity', 'eigenvalues-eigenvectors', 'permutations', 'set-theory', 'induction', 'modular-arithmetic', 'logarithms', 'recurrence-relations', 'terminology', 'modules', 'representation-theory', 'operator-theory', 'asymptotics', 'arithmetic', 'random-variables', 'algebraic-number-theory', 'manifolds', 'power-series', 'convex-analysis', 'computer-science', 'hilbert-spaces', 'galois-theory', 'binomial-coefficients', 'improper-integrals', 'differential-topology', 'definition', 'contest-math', 'vectors', 'banach-spaces', 'self-learning', 'special-functions', 'divisibility', 'exponential-function', 'taylor-expansion', 'lie-groups', 'diophantine-equations', 'lebesgue-integral', 'normal-distribution', 'fourier-series', 'ideals', 'dynamical-systems', 'euclidean-geometry', 'physics', 'lie-algebras', 'determinant', 'compactness', 'recreational-mathematics', 'analytic-geometry', 'roots', 'systems-of-equations', 'norm', 'circle', 'riemannian-geometry', 'relations', 'education', 'examples-counterexamples', 'graphing-functions', 'intuition', 'sobolev-spaces', 'indefinite-integrals', 'triangle', 'convex-optimization', 'exponentiation', 'markov-chains', 'partial-derivative', 'finite-fields'];
+  // Tags with 500+ questions: 
+  var Popular = ['calculus', 'real-analysis', 'linear-algebra', 'probability', 'abstract-algebra', 'integration', 'sequences-and-series', 'general-topology', 'combinatorics', 'matrices', 'complex-analysis', 'group-theory', 'algebra-precalculus', 'analysis', 'geometry', 'functional-analysis', 'number-theory', 'differential-equations', 'elementary-number-theory', 'limits', 'probability-theory', 'measure-theory', 'statistics', 'multivariable-calculus', 'functions', 'discrete-mathematics', 'elementary-set-theory', 'trigonometry', 'algebraic-geometry', 'differential-geometry', 'derivatives', 'inequality', 'reference-request', 'logic', 'polynomials', 'graph-theory', 'probability-distributions', 'ring-theory', 'pde', 'algebraic-topology', 'proof-strategy', 'convergence', 'commutative-algebra', 'optimization', 'proof-verification', 'vector-spaces', 'definite-integrals', 'soft-question', 'complex-numbers', 'algorithms', 'summation', 'metric-spaces', 'stochastic-processes', 'finite-groups', 'numerical-methods', 'notation', 'category-theory', 'prime-numbers', 'fourier-analysis', 'field-theory', 'proof-writing', 'continuity', 'eigenvalues-eigenvectors', 'permutations', 'induction', 'set-theory', 'modular-arithmetic', 'logarithms', 'recurrence-relations', 'terminology', 'modules', 'representation-theory', 'operator-theory', 'asymptotics', 'arithmetic', 'random-variables', 'manifolds', 'algebraic-number-theory', 'power-series', 'convex-analysis', 'computer-science', 'hilbert-spaces', 'galois-theory', 'binomial-coefficients', 'improper-integrals', 'differential-topology', 'definition', 'contest-math', 'vectors', 'banach-spaces', 'self-learning', 'special-functions', 'exponential-function', 'divisibility', 'taylor-expansion', 'lie-groups', 'diophantine-equations', 'lebesgue-integral', 'fourier-series', 'normal-distribution', 'ideals', 'euclidean-geometry', 'dynamical-systems', 'physics', 'lie-algebras', 'determinant', 'compactness', 'analytic-geometry', 'recreational-mathematics', 'roots', 'systems-of-equations', 'riemannian-geometry', 'norm', 'circle', 'relations', 'education', 'examples-counterexamples', 'graphing-functions', 'intuition', 'sobolev-spaces', 'indefinite-integrals', 'triangle', 'convex-optimization', 'exponentiation', 'markov-chains', 'partial-derivative', 'combinations', 'finite-fields', 'lebesgue-measure', 'inverse', 'homological-algebra', 'problem-solving', 'approximation', 'vector-analysis', 'stochastic-calculus', 'expectation', 'transformation', 'generating-functions', 'puzzle', 'matlab', 'normed-spaces', 'order-theory', 'propositional-calculus', 'laplace-transform', 'linear-programming', 'math-history', 'homology-cohomology', 'cardinals', 'computational-complexity', 'mathematical-physics', 'solution-verification', 'inner-product-space', 'conic-sections', 'lp-spaces', 'model-theory', 'analytic-number-theory', 'contour-integration', 'tensor-products', 'abelian-groups', 'brownian-motion', 'functional-equations', 'homotopy-theory', 'factorial', 'factoring', 'extension-field', 'quadratics', 'game-theory', 'coordinate-systems', 'numerical-linear-algebra', 'closed-form', '3d', 'boolean-algebra', 'algebraic-curves', 'uniform-convergence', 'predicate-logic', 'computability', 'statistical-inference', 'distribution-theory', 'regression', 'equivalence-relations', 'rotations', 'surfaces', 'elliptic-curves', 'calculus-of-variations', 'polar-coordinates', 'conditional-probability', 'infinity', 'big-list', 'matrix-equations', 'axiom-of-choice', 'operator-algebras', 'connectedness', 'spectral-theory', 'book-recommendation', 'parametric', 'linear-transformations', 'differential-forms', 'fractions', 'congruences', 'harmonic-analysis', 'convolution', 'finance', 'random', 'tensors', 'real-numbers', 'volume', 'gamma-function', 'absolute-value', 'fibonacci-numbers', 'signal-processing', 'martingales', 'complex-geometry', 'cryptography', 'interpolation', 'irreducible-polynomials', 'riemann-zeta', 'sheaf-theory', 'nonlinear-optimization', 'area', 'residue-calculus', 'topological-groups', 'radicals', 'irrational-numbers', 'formal-languages', 'projective-geometry', 'alternative-proof', 'c-star-algebras', 'mathematical-modeling', 'first-order-logic', 'automata', 'vector-bundles', 'smooth-manifolds', 'partitions', 'recursion', 'stochastic-integrals', 'riemann-surfaces', 'symmetric-groups', 'schemes', 'quadratic-forms', 'ordinals', 'divergent-series', 'coding-theory', 'information-theory', 'lagrange-multiplier', 'recursive-algorithms', 'math-software', 'markov-process', 'group-actions', 'trees', 'harmonic-functions', 'rational-numbers', 'economics', 'machine-learning', 'applications', 'random-walk', 'banach-algebras', 'limsup-and-liminf', 'cyclic-groups', 'plane-curves', 'binary', 'matrix-calculus', 'hyperbolic-geometry', 'average', 'weak-convergence', 'diagonalization', 'fixed-point-theorems', 'p-adic-number-theory', 'products', 'computational-geometry', 'pi', 'standard-deviation', 'dice', 'topological-vector-spaces', 'epsilon-delta', 'lattice-orders', 'prime-factorization', 'noncommutative-algebra', 'bayesian', 'control-theory'];
 
   // Okay as a top tag: either popular or otherwise sufficient
-  var Okay = Popular.concat(['automata', 'analytic-number-theory', 'boolean-algebra', 'calculus-of-variations', 'coding-theory', 'combinatorial-game-theory', 'computability', 'computational-complexity', 'complex-geometry', 'context-free-grammar', 'coordinate-systems', 'descriptive-statistics', 'equivalence-relations', 'finance', 'formal-languages', 'game-theory', 'harmonic-analysis', 'homological-algebra', 'homology-cohomology', 'homotopy-theory', 'irrational-numbers', 'laplace-transform', 'linear-programming', 'math-history', 'mathematical-physics', 'model-theory', 'numerical-linear-algebra', 'order-theory', 'percentages', 'predicate-logic', 'propositional-calculus', 'puzzle', 'real-numbers', 'regular-language', 'stochastic-calculus']);  
+  var Okay = Popular.concat(['combinatorial-game-theory', 'context-free-grammar', 'descriptive-statistics', 'finite-automata', 'percentages', 'regular-expressions', 'regular-language']);     
   
   // These should not be used on their own
-  var Vague = ['advice', 'big-list', 'book-recommendation', 'contest-math', 'definition', 'examples-counterexamples', 'norm', 'notation', 'proof-strategy', 'proof-verification', 'proof-writing', 'reference-request', 'soft-question', 'terminology', 'transformation'];
+  var Vague = ['advice', 'big-list', 'book-recommendation', 'calculator', 'contest-math', 'definition', 'examples-counterexamples', 'gmat-exam', 'gre-exam', 'norm', 'notation', 'proof-strategy', 'proof-verification', 'proof-writing', 'reference-request', 'soft-question', 'terminology', 'transformation'];
   
   // Suggest Replacements for these: 
   var ReplaceTag = ['analysis']; 
-  var Replacements = [['real-analysis', 'complex-analysis', 'functional-analysis', 'fourier-analysis', 'measure-theory', 'calculus-of-variations', 'calculus', 'multivariable-calculus']];
+  var Replacements = [['real-analysis', 'complex-analysis', 'functional-analysis', 'fourier-analysis', 'harmonic-analysis', 'measure-theory', 'calculus-of-variations', 'calculus', 'multivariable-calculus', 'pde']];
   var ReplaceText = ['Consider replacing (analysis) with a more specific tag for the relevant branch of analysis. ']; 
   
   startup();
@@ -75,7 +75,7 @@ function processQuestion(id) {
       if (postData.owner.user_id) {
         uId = parseInt(postData.owner.user_id, 10);
       }
-      if (comment.text.length > 0 && postData.owner.reputation == 1 && uId > MaxUser - 3) {
+      if (comment.text.length > 0 && (postData.owner.reputation == 1 || postData.owner.reputation == 101) && uId > MaxUser - 10) {
         comment.text = 'Welcome to Math.SE, ' + postData.owner.display_name + '. ' + comment.text;
       }
       sendComment(comment); 
@@ -88,7 +88,7 @@ function processQuestion(id) {
 
 
 function commentOnTitle(title, comment) {
-  var badWords = title.match(/\b(anyone|difficult|doubt|hard|help|interesting|please|query|question|someone|struggling|stuck|task|tough)\b/ig); 
+  var badWords = title.match(/\b(anyone|challenging|difficult|doubt|help|interesting|please|query|question|someone|struggling|stuck|task|tough)\b/ig); 
   var punctuationMatch = title.match(/\w\?{2,}/ig);
   var tallReasons = []; 
   if (badWords && title.length <= 70) {
@@ -106,9 +106,6 @@ function commentOnTitle(title, comment) {
   if (/\\displaystyle/.test(title)) {
     tallReasons.push('remove `\\displaystyle`');
   }  
-  if (/(\\int|\\sum)\s*\\limits/.test(title)) {
-    tallReasons.push('remove `\\limits`');
-  }
   if (tallReasons.length > 0) {
     comment.text = comment.text + 'Tall formulas in titles break the layout of question lists. Please ' + tallReasons.join(', ') + ' in the title. ';
   }
@@ -122,6 +119,7 @@ function commentOnTitle(title, comment) {
 function commentOnBody(body, comment) {
   body = body.replace(/<pre>[\s\S]*?<\/pre>/g, "pre");
   body = body.replace(/<code>[\s\S]*?<\/code>/g, "code");
+  body = body.replace(/<a[\s\S]*?<\/a>/g, "link");
   var math = body.match(/&lt;|&gt;|[*^+_]|\/\d|\b(sin|cos|tan|exp|log|ln|sqrt|pi)\b/g);
   if (!/\$/.test(body) && math && math.length >= 5) {
     comment.text = comment.text + 'This site uses MathJax for formulas: see [math notation guide](//math.stackexchange.com/help/notation). ';
