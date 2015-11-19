9d3f817114683ac716ee6ff8bd71e1310983c045
Normal Human <normalhuman@users.noreply.github.com>
2015-11-18 22:58:36 -0500

    keep better track of tag reports

--- EarlyWarning.user.js
+++ EarlyWarning.user.js
@@ -5,7 +5,7 @@
 // @match       *://chat.stackexchange.com/rooms/30985/normal-chatroom
 // @grant       none
 // @run-at      document-end
-// @version     15.11.7
+// @version     15.11.8
 // ==/UserScript==
 
 // runs only if the browser is pointed at ://chat.stackexchange.com/rooms/30985/normal-chatroom#bot
@@ -16,27 +16,27 @@ if (window.location.hash === '#bot') {
   var ApiKey = 'vURDLnkgkrLc7qAq)D89tA(('; 
   var Site = 'math';
   var SiteUrl = 'http://math.stackexchange.com';
-  var Commented = [], MaxUser = 0, CommentId, PostId, GlobalComment, LastChatMessage = 0;
+  var Commented = [], MaxUser = 0, CommentId, PostId, GlobalComment, LastChatMessage = 0, ReportedTags = [];
   
   // Tags with 500+ questions: 
   var Popular = ['calculus', 'real-analysis', 'linear-algebra', 'probability', 'abstract-algebra', 'integration', 'sequences-and-series', 'general-topology', 'combinatorics', 'matrices', 'complex-analysis', 'group-theory', 'algebra-precalculus', 'analysis', 'geometry', 'functional-analysis', 'number-theory', 'differential-equations', 'elementary-number-theory', 'limits', 'probability-theory', 'measure-theory', 'statistics', 'multivariable-calculus', 'functions', 'discrete-mathematics', 'elementary-set-theory', 'trigonometry', 'algebraic-geometry', 'differential-geometry', 'derivatives', 'inequality', 'reference-request', 'logic', 'polynomials', 'graph-theory', 'probability-distributions', 'ring-theory', 'pde', 'algebraic-topology', 'proof-strategy', 'convergence', 'commutative-algebra', 'optimization', 'proof-verification', 'vector-spaces', 'definite-integrals', 'soft-question', 'complex-numbers', 'algorithms', 'summation', 'metric-spaces', 'stochastic-processes', 'finite-groups', 'numerical-methods', 'notation', 'category-theory', 'prime-numbers', 'fourier-analysis', 'field-theory', 'proof-writing', 'continuity', 'eigenvalues-eigenvectors', 'permutations', 'induction', 'set-theory', 'modular-arithmetic', 'logarithms', 'recurrence-relations', 'terminology', 'modules', 'representation-theory', 'operator-theory', 'asymptotics', 'arithmetic', 'random-variables', 'manifolds', 'algebraic-number-theory', 'power-series', 'convex-analysis', 'computer-science', 'hilbert-spaces', 'galois-theory', 'binomial-coefficients', 'improper-integrals', 'differential-topology', 'definition', 'contest-math', 'vectors', 'banach-spaces', 'self-learning', 'special-functions', 'exponential-function', 'divisibility', 'taylor-expansion', 'lie-groups', 'diophantine-equations', 'lebesgue-integral', 'fourier-series', 'normal-distribution', 'ideals', 'euclidean-geometry', 'dynamical-systems', 'physics', 'lie-algebras', 'determinant', 'compactness', 'analytic-geometry', 'recreational-mathematics', 'roots', 'systems-of-equations', 'riemannian-geometry', 'norm', 'circle', 'relations', 'education', 'examples-counterexamples', 'graphing-functions', 'intuition', 'sobolev-spaces', 'indefinite-integrals', 'triangle', 'convex-optimization', 'exponentiation', 'markov-chains', 'partial-derivative', 'combinations', 'finite-fields', 'lebesgue-measure', 'inverse', 'homological-algebra', 'problem-solving', 'approximation', 'vector-analysis', 'stochastic-calculus', 'expectation', 'transformation', 'generating-functions', 'puzzle', 'matlab', 'normed-spaces', 'order-theory', 'propositional-calculus', 'laplace-transform', 'linear-programming', 'math-history', 'homology-cohomology', 'cardinals', 'computational-complexity', 'mathematical-physics', 'solution-verification', 'inner-product-space', 'conic-sections', 'lp-spaces', 'model-theory', 'analytic-number-theory', 'contour-integration', 'tensor-products', 'abelian-groups', 'brownian-motion', 'functional-equations', 'homotopy-theory', 'factorial', 'factoring', 'extension-field', 'quadratics', 'game-theory', 'coordinate-systems', 'numerical-linear-algebra', 'closed-form', '3d', 'boolean-algebra', 'algebraic-curves', 'uniform-convergence', 'predicate-logic', 'computability', 'statistical-inference', 'distribution-theory', 'regression', 'equivalence-relations', 'rotations', 'surfaces', 'elliptic-curves', 'calculus-of-variations', 'polar-coordinates', 'conditional-probability', 'infinity', 'big-list', 'matrix-equations', 'axiom-of-choice', 'operator-algebras', 'connectedness', 'spectral-theory', 'book-recommendation', 'parametric', 'linear-transformations', 'differential-forms', 'fractions', 'congruences', 'harmonic-analysis', 'convolution', 'finance', 'random', 'tensors', 'real-numbers', 'volume', 'gamma-function', 'absolute-value', 'fibonacci-numbers', 'signal-processing', 'martingales', 'complex-geometry', 'cryptography', 'interpolation', 'irreducible-polynomials', 'riemann-zeta', 'sheaf-theory', 'nonlinear-optimization', 'area', 'residue-calculus', 'topological-groups', 'radicals', 'irrational-numbers', 'formal-languages', 'projective-geometry', 'alternative-proof', 'c-star-algebras', 'mathematical-modeling', 'first-order-logic', 'automata', 'vector-bundles', 'smooth-manifolds', 'partitions', 'recursion', 'stochastic-integrals', 'riemann-surfaces', 'symmetric-groups', 'schemes', 'quadratic-forms', 'ordinals', 'divergent-series', 'coding-theory', 'information-theory', 'lagrange-multiplier', 'recursive-algorithms', 'math-software', 'markov-process', 'group-actions', 'trees', 'harmonic-functions', 'rational-numbers', 'economics', 'machine-learning', 'applications', 'random-walk', 'banach-algebras', 'limsup-and-liminf', 'cyclic-groups', 'plane-curves', 'binary', 'matrix-calculus', 'hyperbolic-geometry', 'average', 'weak-convergence', 'diagonalization', 'fixed-point-theorems', 'p-adic-number-theory', 'products', 'computational-geometry', 'pi', 'standard-deviation', 'dice', 'topological-vector-spaces', 'epsilon-delta', 'lattice-orders', 'prime-factorization', 'noncommutative-algebra', 'bayesian', 'control-theory'];
 
   // Okay as a top tag: either popular or otherwise sufficient
-  var Okay = Popular.concat(['actuarial-science', 'combinatorial-game-theory', 'context-free-grammar', 'data-analysis', 'descriptive-set-theory', 'descriptive-statistics', 'finite-automata', 'geometric-topology', 'group-cohomology', 'percentages', 'regular-expressions', 'regular-language', 'symplectic-geometry']);   
+  var Okay = Popular.concat(['actuarial-science', 'article-writing', 'combinatorial-game-theory', 'context-free-grammar', 'data-analysis', 'descriptive-set-theory', 'descriptive-statistics', 'finite-automata', 'geometric-topology', 'group-cohomology', 'percentages', 'regular-expressions', 'regular-language', 'spherical-geometry', 'splitting-field', 'symplectic-geometry', 'transcendental-numbers', 'word-problem']);   
   
   // These should not be used on their own
-  var Vague = ['advice', 'alternative-proof', 'big-list', 'book-recommendation', 'calculator', 'contest-math', 'definition', 'examples-counterexamples', 'fake-proofs', 'gmat-exam', 'gre-exam', 'norm', 'notation', 'online-resources', 'problem-solving', 'proof-explanation', 'proof-strategy', 'proof-verification', 'proof-writing', 'reference-request', 'soft-question', 'terminology', 'transformation'];
+  var Vague = ['advice', 'alternative-proof', 'big-list', 'book-recommendation', 'calculator', 'contest-math', 'definition', 'examples-counterexamples', 'fake-proofs', 'gmat-exam', 'gre-exam', 'learning', 'norm', 'notation', 'online-resources', 'problem-solving', 'proof-explanation', 'proof-strategy', 'proof-verification', 'proof-writing', 'reference-request', 'soft-question', 'terminology', 'transformation'];
   
   // Suggest Replacements for these: 
   var ReplaceTag = ['analysis', 'intersection-theory']; 
-  var Replacements = [['real-analysis', 'complex-analysis', 'functional-analysis', 'fourier-analysis', 'harmonic-analysis', 'measure-theory', 'calculus-of-variations', 'calculus', 'multivariable-calculus', 'pde'], 
+  var Replacements = [['real-analysis', 'complex-analysis', 'functional-analysis', 'fourier-analysis', 'harmonic-analysis', 'measure-theory', 'calculus-of-variations', 'calculus', 'multivariable-calculus', 'pde', 'convex-analysis'], 
                       ['algebraic-geometry', 'algebraic-curves', 'algebraic-topology']];
   var ReplaceText = ['Consider replacing (analysis) with a more specific tag for the relevant branch of analysis. ',
                     'The tag (intersection-theory) should not be used just because intersections are involved; it is meant for a branch of algebraic geometry and topology.']; 
     
   
   // Post in chat when all these tags are present, even if not commenting
-  var ManualReview = [['proof-strategy'], ['proof-verification'], ['proof-explanation'], ['proof-writing'], ['theorem-provers'], ['self-learning'], ['stability-theory'], ['pde', 'differential-equations'], ['maple'], ['mathematica'], ['matlab']]; 
+  var ManualReview = [['learning'], ['proof-strategy'], ['proof-verification'], ['proof-explanation'], ['proof-writing'], ['theorem-provers'], ['self-learning'], ['stability-theory'], ['pde', 'differential-equations'], ['maple'], ['mathematica'], ['matlab']]; 
 
   startup();
 }
@@ -101,7 +101,7 @@ function processQuestion(id) {
 
 function commentOnTitle(title, comment) {
   var badWords = title.match(/\b(anybody|anyone|beautiful|challenging|difficult|doubt|help|interesting|nice|please|query|question|somebody|someone|struggling|stuck|task|tough)\b/ig); 
-  var suspectWords = title.match(/(advanced|basic|beginning|confus|easy|elegant|elementary|exercise|\bgre\b|hard\b|homework|problem|\bsat\b|stump|tricky|troubl|ugly|urgent|\bvery|weird|wrong|!\?|\?\?|pmatrix|\\limits|\\begin|\b[0-9]$)/ig);  
+  var suspectWords = title.match(/(advanced|basic|beginning|confus|easy|elegant|elementary|exercise|\bgre\b|hard\b|homework|problem|\bsat\b|stump|tricky|troubl|ugly|urgent|\bvery|weird|wrong|!\?|\?\?|pmatrix|\\limits|\\begin)/ig);  
   var tallReasons = [], prepWords; 
   if (badWords && title.length < 60) {
     prepWords = '*' + badWords.join(', ').toLowerCase() + '*';
@@ -128,6 +128,9 @@ function commentOnTitle(title, comment) {
     prepWords = [].concat(badWords).concat(suspectWords).filter(function(a) {return a;}).join(', ').toLowerCase();
     comment.chat = comment.chat + 'Title contains *' + prepWords + '*. ';
   }
+  if (/^\D*\d$/.test(title)) {
+    comment.chat = comment.chat + 'Title ends with a digit. ';
+  }
   return comment;
 }
 
@@ -146,7 +149,7 @@ function commentOnBody(body, comment) {
   if (body.length < 200) {
     comment.chat = comment.chat + 'Short question. ';
   }
-  if (badWords) {
+  if (badWords && body.length < 500) {
     comment.chat = comment.chat + 'Question contains *' + badWords.join(', ').toLowerCase() + '*. ';
   }  
   return comment;
@@ -157,7 +160,7 @@ function commentOnTags(tags, comment) {
   var obscureTags = ['Consider adding a tag for a broader subject area to which the question belongs. ', 'Questions tend to get more attention when they have a tag for a broad area of mathematics relevant to the question. '];  
   var topTag = tags[0]; 
   var replacing = ReplaceTag.indexOf(topTag);
-  if (tags.indexOf('self-learning') > -1 && arraysDisjoint(tags, ['soft-question', 'career-development', 'education', 'teaching', 'advice', 'book-recommendation', 'reference-request', 'big-list'])) {
+  if (tags.indexOf('self-learning') > -1 && arraysDisjoint(tags, ['soft-question', 'career-development', 'education', 'teaching', 'advice', 'learning', 'book-recommendation', 'reference-request', 'big-list'])) {
     comment.text = comment.text + "Please don't use (self-learning) tag just because you were self-studying. This tag is only for questions *about the process of self-studying*. ";
   }
   if (replacing > -1 && arraysDisjoint(tags, Replacements[replacing])) {
@@ -168,6 +171,8 @@ function commentOnTags(tags, comment) {
   }
   else if (Okay.indexOf(topTag) === -1) {
     comment.text = comment.text + obscureTags[Math.floor(obscureTags.length*Math.random())] + 'Some of [these tags](//math.stackexchange.com/tags/'+topTag+'/info#h-related-tags) might fit. ';
+    ReportedTags.push(topTag);
+    console.log('Tags reported so far: ' + ReportedTags.join(', '));
   }
   for (var i = 0; i < ManualReview.length; i++) {
     if (arrayContained(ManualReview[i], tags)) {
